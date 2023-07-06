9fbe0ee79384494b732b8d7b7271b05601433e9f
John Niesman <john-niesman@uiowa.edu>
2023-07-06 09:30:19 -0500

    case switching in python

---
+++ InputHandling.py
@@ -0,0 +1,16 @@
+# Clean way to handle user inputs
+
+def do_this():
+    print('Doing this')
+
+def do_that():
+    print('Doing that')
+
+match input('Do this or that? '):
+    case 'this':
+        do_this()
+    case 'that':
+        do_that()
+    case _:
+        print('Invalid Input')
+
